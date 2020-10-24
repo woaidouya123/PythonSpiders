79736e63f56d970eb7a30edc84834276ba44061e
woaidouya123 <cfeigood@qq.com>
2020-10-24 23:26:29 +0800

    csdnblog爬虫修复

--- blogviewer/csdnblog.py
+++ blogviewer/csdnblog.py
@@ -1,13 +1,14 @@
 import requests
 from lxml import etree
 import time
+from datetime import datetime
 import sys
  
  
 #定义请求函数
 def get_html(url,headers):
-    response = requests.get(url , headers)
-    return response.content
+    response = requests.get(url , headers=headers)
+    return response.text
  
  
 #定义解析函数
@@ -18,18 +19,26 @@ def resolve_html(content):
  
 #定义主函数
 def start():
-   domain = 'https://blog.csdn.net/woaidouya123'
-   headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:61.0) Gecko/20100101 Firefox/61.0'}
-   try:
-    content = get_html(domain,headers)
-    pages = resolve_html(content).xpath('//*[@id="mainBox"]/main/div[2]/div/h4/a/@href')
-    for page in pages:
-     page_content = get_html(page,headers)
-     time.sleep(3)
-     html = resolve_html(page_content)
-     title = html.xpath('//*[@id="mainBox"]/main/div[1]/div/div/div[1]/h1')[0].text
-     print(title,end=",")
-   except:
-    print("Unexpected error:", sys.exc_info()[0])
+   print("脚本运行时间：",datetime.now().strftime("%Y-%m-%d %H:%M:%S"),"———— ———— ——— ——— ——— ——— ——— ——— ———— ————")
+   page_list = 2
+   for p in range(page_list):
+     domain = 'https://blog.csdn.net/woaidouya123/article/list/'+str(p+1)
+     print(domain)
+     headers = {'User-Agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.106 Safari/537.36',}
+     try:
+      content = get_html(domain,headers)
+      pages = resolve_html(content).xpath('//*[@id="articleMeList-blog"]/div[2]/div/h4/a/@href')
+      print(pages)
+      for page in pages:
+       page_content = get_html(page,headers)
+       time.sleep(3)
+       html = resolve_html(page_content)
+       title = html.xpath('//*[@id="mainBox"]/main/div[1]/div/div/div[1]/h1')[0].text
+       print(title,end=",")
+     except:
+      print("Unexpected error:", sys.exc_info()[0])
+
+if __name__ == '__main__':
+    start()
    
    
